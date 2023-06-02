8c26dcbb0b5f630465a92011928b90c891f9b391
Mohit Raykwar <mohit.raykwar@statxo.com>
2023-06-02 16:42:17 +0530

    nodemailer.

--- src/controller/login.js
+++ src/controller/login.js
@@ -7,11 +7,12 @@ const jwt = require('jsonwebtoken');
 const nodemailer = require("nodemailer");
 
 var transport = nodemailer.createTransport({
-    host: "smtp.office365.com",
+    host: "smtp.render.com",
     port: 587, // secure SMTP-587
-    secureConnection:true,
+    // secureConnection:true,
     requireTLS: true,
     tls: { ciphers: "SSLv3" },
+    secure:false,
     auth: {
         user: process.env.STATXO_MAIL,
         pass: process.env.STATXO_MAIL_PASS,
