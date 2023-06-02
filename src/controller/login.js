5069648c5005e92d85ecd25e241cd4ce883a6626
Mohit Raykwar <mohit.raykwar@statxo.com>
2023-06-02 13:41:00 +0530

    nodemailer

--- src/controller/login.js
+++ src/controller/login.js
@@ -9,9 +9,9 @@ const nodemailer = require("nodemailer");
 var transport = nodemailer.createTransport({
     host: "smtp.office365.com",
     port: 465, // secure SMTP - 465 -587
-    secureConnection: true,
-    requireTLS: true,
-    tls: { ciphers: "SSLv3" },
+    secure: true,
+    // requireTLS: true,
+    // tls: { ciphers: "SSLv3" },
     auth: {
         user: process.env.STATXO_MAIL,
         pass: process.env.STATXO_MAIL_PASS,
