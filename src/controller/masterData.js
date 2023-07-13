50846f3f6c28b07ff9aaeeab253f34f42b176449
Mohit Raykwar <mohit.raykwar@statxo.com>
2023-07-13 18:02:31 +0530

    master data

--- src/controller/masterData.js
+++ src/controller/masterData.js
@@ -12,7 +12,6 @@ const SpendData = async function (req, res) {
             console.log("connected");
             return pool.request().query(`SELECT * FROM [DevOps].[SpendData]`);
         });
-        console.log(spend.recordset);
         return res.status(200).send({ result:spend.recordset, message:"spend data fetched successfully" });
     } catch (e) {
         res.status(500).send({ status: false, message: e.message });
@@ -26,7 +25,6 @@ const SavingData = async function (req, res) {
             console.log("connected");
             return pool.request().query(`SELECT * FROM [DevOps].[SavingData_2]`);
         });
-        console.log(save.recordset);
         return res.status(200).send({ result:save.recordset, message:"saving data fetched successfully" });
     } catch (e) {
         res.status(500).send({ status: false, message: e.message });
