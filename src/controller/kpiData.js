b6bcd3784bd235fa6a6227d96f97ffeeac22fc0e
Mohit Raykwar <mohit.raykwar@statxo.com>
2023-07-19 18:44:03 +0530

    --

--- src/controller/kpiData.js
+++ src/controller/kpiData.js
@@ -4,10 +4,103 @@ const sql = require("mssql");
 
 const getKpi = async function (req, res) {
     try {
+        // let {email} = req.query;
+        // var poolConnection = await sql.connect(config);
+        // console.log("connected");
+        // console.log("KPI Loading");
+        // let data=[];
+
+        // //spend
+        // let sp = await poolConnection.request().query(`SELECT SUM(AmountEUR),COUNT(DISTINCT CompanyName),COUNT(DISTINCT Supplier_Key),COUNT(CompanyName),COUNT(DISTINCT ReportingLevel4),MAX(YearMonth),MIN(YearMonth),COUNT(DISTINCT Entity_Country)
+        // FROM [DevOps].[SpendData]`);
+
+        // // saving
+        // let sv = await poolConnection.request().query(`SELECT SUM(CALC_AmountEUR_YTD_TY),COUNT(DISTINCT CompanyPrimaryCluster),SUM(CALC_PriceVariance_YTD),MAX(YearMonth),MIN(YearMonth),COUNT(DISTINCT Entity_RegionP)
+        // FROM [DevOps].[SavingData_2]`);
+
+        // // //action
+        // let ac = await poolConnection.request().query(`SELECT SUM(AmountEUR),COUNT(DISTINCT CompanyName),COUNT(DISTINCT VendorNameHarmonized),MAX(YearMonth),MIN(YearMonth),COUNT(DISTINCT ActionName),COUNT(DISTINCT Entity_Country),COUNT(case when Status = 'Pending' then 1 else null end)
+        // FROM [DevOps].[ActionTracking_test]`);
+
+        // let ac2 = await poolConnection.request().query(`SELECT SUM([AmountEUR(Pre)]),SUM([AmountEUR(Post)])
+        // FROM [DevOps].[ActionTracking_test_upd]`);
+        // ac2 = ac2.recordsets[0][0][""][1]-ac2.recordsets[0][0][""][0];
+
+        // //help
+        // var help = await poolConnection.request().query(`SELECT COUNT(Status),COUNT(case when Status = 'Pending' then 1 else null end),COUNT(case when Status = 'In Progress' then 1 else null end),COUNT(case when Status = 'Rejected' then 1 else null end),COUNT(case when Status = 'Successfull' then 1 else null end)
+        // FROM [DevOps].[Help_Desk_Table] WHERE Email = '${email}'`);
+        
+        // var lasthelp = await poolConnection.request().query(`SELECT Date
+        // FROM [DevOps].[Help_Desk_Table] WHERE Email = '${email}'`);
+        // lasthelp = lasthelp.recordsets[0];
+        // let mn=0;
+        // let yr=0;
+        // for(let i=0; i<lasthelp.length; i++){
+        //     let date = lasthelp[i].Date;
+        //     date = date.split("/");
+        //     let month = date[1];
+        //     let year = date[2];
+        //     if(yr<year){
+        //         mn = month;
+        //         yr = year;
+        //     }
+        //     else if(yr == year && month > mn){
+        //         mn = month;
+        //     }
+        // }
+        // let lhelp = yr+""+mn;
+
+        // data.push(
+        //     {
+        //         totalSpend:sp.recordsets[0][0][""][0],
+        //         spendEntity:sp.recordsets[0][0][""][1],
+        //         spendSupplier:sp.recordsets[0][0][""][2],
+        //         spendTransaction:sp.recordsets[0][0][""][3],
+        //         spendL4Category:sp.recordsets[0][0][""][4],
+        //         spendTimeRange:[sp.recordsets[0][0][""][6],sp.recordsets[0][0][""][5]],
+        //         spendCountry:sp.recordsets[0][0][""][7],
+        //         totalSave:sv.recordsets[0][0][""][0],
+        //         saveEntity:sv.recordsets[0][0][""][1],
+        //         saveVariance1:sv.recordsets[0][0][""][2],
+        //         saveTimeRange:[sv.recordsets[0][0][""][4],sv.recordsets[0][0][""][3]],
+        //         saveRegion:sv.recordsets[0][0][""][5],
+        //         totalAction:ac.recordsets[0][0][""][0],
+        //         actionEntity:ac.recordsets[0][0][""][1],
+        //         actionSupplier:ac.recordsets[0][0][""][2],
+        //         actionTimeRange:[ac.recordsets[0][0][""][4],ac.recordsets[0][0][""][3]],
+        //         actionNumber:ac.recordsets[0][0][""][5],
+        //         actionCountry:ac.recordsets[0][0][""][6],
+        //         actionUnderCons:ac.recordsets[0][0][""][7],
+        //         actionSaving:ac2,
+        //         helpTicket:help.recordsets[0][0][""][0],
+        //         pending:help.recordsets[0][0][""][1],
+        //         progress:help.recordsets[0][0][""][2],
+        //         reject:help.recordsets[0][0][""][3],
+        //         success:help.recordsets[0][0][""][4],
+        //         helpTime:lhelp,
+        //     }
+        // );
+
+        poolConnection.close();
+        console.log("disconnected");
+        // return res.status(200).send({ result: JSON.stringify(data), message:"kpi fetched successfully" });
+    } catch (e) {
+        res.status(500).send({ status: false, message: e.message });
+    }
+};
+
+const getChart = async function (req, res) {
+    try {
         let {email} = req.query;
+        function formatCompactNumber(number) {
+            number = number+"";
+            num = number.split(".");
+            return Number(num[0]);
+            }
+
         var poolConnection = await sql.connect(config);
         console.log("connected");
-        console.log("KPI Loading");
+
         let data=[];
 
         //spend
@@ -50,57 +143,6 @@ const getKpi = async function (req, res) {
         }
         let lhelp = yr+""+mn;
 
-        data.push(
-            {
-                totalSpend:sp.recordsets[0][0][""][0],
-                spendEntity:sp.recordsets[0][0][""][1],
-                spendSupplier:sp.recordsets[0][0][""][2],
-                spendTransaction:sp.recordsets[0][0][""][3],
-                spendL4Category:sp.recordsets[0][0][""][4],
-                spendTimeRange:[sp.recordsets[0][0][""][6],sp.recordsets[0][0][""][5]],
-                spendCountry:sp.recordsets[0][0][""][7],
-                totalSave:sv.recordsets[0][0][""][0],
-                saveEntity:sv.recordsets[0][0][""][1],
-                saveVariance1:sv.recordsets[0][0][""][2],
-                saveTimeRange:[sv.recordsets[0][0][""][4],sv.recordsets[0][0][""][3]],
-                saveRegion:sv.recordsets[0][0][""][5],
-                totalAction:ac.recordsets[0][0][""][0],
-                actionEntity:ac.recordsets[0][0][""][1],
-                actionSupplier:ac.recordsets[0][0][""][2],
-                actionTimeRange:[ac.recordsets[0][0][""][4],ac.recordsets[0][0][""][3]],
-                actionNumber:ac.recordsets[0][0][""][5],
-                actionCountry:ac.recordsets[0][0][""][6],
-                actionUnderCons:ac.recordsets[0][0][""][7],
-                actionSaving:ac2,
-                helpTicket:help.recordsets[0][0][""][0],
-                pending:help.recordsets[0][0][""][1],
-                progress:help.recordsets[0][0][""][2],
-                reject:help.recordsets[0][0][""][3],
-                success:help.recordsets[0][0][""][4],
-                helpTime:lhelp,
-            }
-        );
-
-        poolConnection.close();
-        console.log("disconnected");
-        return res.status(200).send({ result: JSON.stringify(data), message:"kpi fetched successfully" });
-    } catch (e) {
-        res.status(500).send({ status: false, message: e.message });
-    }
-};
-
-const getChart = async function (req, res) {
-    try {
-
-        function formatCompactNumber(number) {
-            number = number+"";
-            num = number.split(".");
-            return Number(num[0]);
-            }
-
-        var poolConnection = await sql.connect(config);
-        console.log("connected");
-
         var spend = await poolConnection.request().query(`SELECT CompanyName,YearMonth,SUM(AmountEUR)
         FROM [DevOps].[SpendData] GROUP BY CompanyName,YearMonth ORDER BY CompanyName,YearMonth ASC`);
         spend = spend.recordsets[0];
@@ -140,9 +182,42 @@ const getChart = async function (req, res) {
                 }
             }
             console.log(ar);
+
+            data.push(
+                {
+                    totalSpend:sp.recordsets[0][0][""][0],
+                    spendEntity:sp.recordsets[0][0][""][1],
+                    spendSupplier:sp.recordsets[0][0][""][2],
+                    spendTransaction:sp.recordsets[0][0][""][3],
+                    spendL4Category:sp.recordsets[0][0][""][4],
+                    spendTimeRange:[sp.recordsets[0][0][""][6],sp.recordsets[0][0][""][5]],
+                    spendCountry:sp.recordsets[0][0][""][7],
+                    totalSave:sv.recordsets[0][0][""][0],
+                    saveEntity:sv.recordsets[0][0][""][1],
+                    saveVariance1:sv.recordsets[0][0][""][2],
+                    saveTimeRange:[sv.recordsets[0][0][""][4],sv.recordsets[0][0][""][3]],
+                    saveRegion:sv.recordsets[0][0][""][5],
+                    totalAction:ac.recordsets[0][0][""][0],
+                    actionEntity:ac.recordsets[0][0][""][1],
+                    actionSupplier:ac.recordsets[0][0][""][2],
+                    actionTimeRange:[ac.recordsets[0][0][""][4],ac.recordsets[0][0][""][3]],
+                    actionNumber:ac.recordsets[0][0][""][5],
+                    actionCountry:ac.recordsets[0][0][""][6],
+                    actionUnderCons:ac.recordsets[0][0][""][7],
+                    actionSaving:ac2,
+                    helpTicket:help.recordsets[0][0][""][0],
+                    pending:help.recordsets[0][0][""][1],
+                    progress:help.recordsets[0][0][""][2],
+                    reject:help.recordsets[0][0][""][3],
+                    success:help.recordsets[0][0][""][4],
+                    helpTime:lhelp,
+                    chart:ar
+                }
+            );
+
         poolConnection.close();
         console.log("disconnected");
-        return res.status(200).send({ result: ar, message:"chart fetched successfully" });
+        return res.status(200).send({ result: data, message:"chart fetched successfully" });
     } catch (e) {
         res.status(500).send({ status: false, message: e.message });
     }
